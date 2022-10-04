b2ee0f93f8592bb9abd7314844e8b026b378437e
fatihmaras <fatihmmaras@gmail.com>
2022-10-04 03:46:34 +0300

    Inheritance Example

---
+++ EB-T9-Inheritance/Hayvan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EB_T9_Inheritance
+{
+    public class Hayvan
+    {
+        public string hayvan_isim;
+        public string hayvan_Cinsiyet;
+        public string hayvan_Rengi;
+        private int hayvan_yas;
+
+
+        public void hayvanBilgisiGoster()
+        {
+            Console.WriteLine("Hayvanın ismi: " + hayvan_isim);
+            Console.WriteLine("Hayvanın Cinsiyeti: " + hayvan_Cinsiyet);
+            Console.WriteLine("Hayvanın Rengi: " + hayvan_Rengi);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ EB-T9-Inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿using EB_T9_Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceGiris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kedi kedi=new Kedi();
+            kedi.hayvan_isim = " Toper";
+            kedi.hayvan_Cinsiyet = " Dişi ";
+            kedi.hayvan_Rengi = "Alaca";
+
+            kedi.hayvanBilgisiGoster();
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
